151dd187d02a67cb09e6d125889c569e699fd7a7
ArturGainutdinov <gajnutdinov.a.l@ft-soft.ru>
2024-02-22 20:50:15 +0500

    изменил файл b

--- src/B.tsx
+++ src/B.tsx
@@ -1,5 +1,10 @@
 import css from "./b-styles.module.css";
 
 export function B() {
-    return <h1 className={css.titleText}>Title B</h1>;
+    return (
+        <div>
+            <p>Hello</p>
+            <h1 className={css.titleText}>Title B</h1>
+        </div>
+    );
 }
